63e6d6b4f993f4acc9e3e480e602155e56ff4d32
Marco <mormando.1746815@studenti.uniroma1.it>
2021-09-19 03:56:14 -0700

    Working on downloader api

--- downloader_api.py
+++ downloader_api.py
@@ -3,12 +3,10 @@ import subprocess, json, sys
 from tqdm import tqdm
 from loguru import logger
 from datetime import datetime
-
-
+from sqlalchemy.sql import text
 from database import *
 import requests
 from time import sleep 
-import threading 
 
 logger.remove()
 logger.add(sys.stderr, level="DEBUG")
@@ -30,17 +28,15 @@ def chunk_list(datas, chunksize):
 
 @logger.catch
 def download_transaction(transaction_id, block_hash, db):
-    logger.debug(f"Downloading transaction: {transaction_id} in blockhash: {block_hash} ")
+
+    if db.query(Transaction).get(transaction_id) is not None:
+        logger.warning(f"Transaction {transaction_id} already exists, skipping it")
+        return
+
+    logger.debug(f"Downloading transaction: {transaction_id} in blockhash: {block_hash}")
     res = requests.get(f"https://chain.so/api/v2/get_tx/BTC/{transaction_id}").json()
 
-    # if res["status"] != "success":
-    #     logger.debug(f"Check response: {res}")
-    #     exit()
-
-
-
     transaction_dic = res['data']
-
     block_network_fee = transaction_dic['network_fee']
 
     transaction = Transaction(id=transaction_id,
@@ -57,7 +53,7 @@ def download_transaction(transaction_id, block_hash, db):
         output_type = output['type']
         
         if output_address == "nonstandard":
-            logger.warning(f"Addr with invalid format found for output in txid: {transaction_id} in blockhash: {block_hash} ")
+            logger.warning(f"Address with invalid format found for output in txid: {transaction_id} in blockhash: {block_hash} ")
             output_address = None
             if output_value == 0:
                 continue
@@ -73,7 +69,7 @@ def download_transaction(transaction_id, block_hash, db):
         input_type = input['type']
         
         if input_address == "nonstandard":
-            logger.warning(f"Addr with invalid format found for input in txid: {transaction_id} in blockhash: {block_hash} ")
+            logger.warning(f"Address with invalid format found for input in txid: {transaction_id} in blockhash: {block_hash} ")
             input_address = None
             if input_value  == 0:
                 continue
@@ -92,31 +88,31 @@ def download_transaction(transaction_id, block_hash, db):
 def download_block(block_height):
     
     res = requests.get(f"https://chain.so/api/v2/get_block/BTC/{block_height}").json()
-
-
-    # if res["status"] != "success":
-    #     logger.debug(f"Check response: {res}")
-    #     exit()
-
+    
     block_dic = res['data']
     block_hash = block_dic['blockhash']
     block_time = datetime.fromtimestamp(int(block_dic['time']))
     block_n_tx = len(block_dic['txs'])
     block_size = block_dic['size']
 
-    block = Block(hash    = block_hash,
-                height    = block_height,
-                time      = block_time,
-                n_tx      = block_n_tx,
-                size      = block_size)
-
     with Session(engine) as db:
-        db.add(block)
-        try:
-            db.commit()
-        except Exception as e :
-            db.rollback()
-            logger.warning(f'Transaction aborted, {e}')
+
+        if db.query(Block).get(block_hash) is not None:
+            logger.warning(f"Block {block_hash} already exists, checking its transactions")
+
+        else:
+            block = Block(hash    = block_hash,
+                        height    = block_height,
+                        time      = block_time,
+                        n_tx      = block_n_tx,
+                        size      = block_size)
+
+            db.add(block)
+            try:
+                db.commit()
+            except Exception as e :
+                db.rollback()
+                logger.warning(f'Transaction aborted, {e}')
             
     
         # for chunk in chunk_list(block_dic['txs'],64):
@@ -125,7 +121,7 @@ def download_block(block_height):
         #     sleep(65)
 
         for transaction_id in block_dic['txs']:
-            download_transaction(transaction_id, block_hash, db )
+            download_transaction(transaction_id, block_hash, db)
             # sleep(0.2)
 
 
@@ -136,13 +132,15 @@ def get_last_block():
 
 if __name__ == '__main__':
 
-    num_blocks = 10
+    # num_blocks = 10
+    num_blocks = 5
     if len(sys.argv) < 2:
         print("Usage: downloader.py [num_blocks] ")
     else:
         num_blocks = int(sys.argv[1])
 
-    last_block = get_last_block()
+    # last_block = get_last_block()
+    last_block = 701125
     
     for block_height in tqdm(range(last_block, last_block-num_blocks, -1)):
         logger.debug(f"Downloading block: {block_height}")
