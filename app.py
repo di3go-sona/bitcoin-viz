3c04e8dd099efe6ac7e044591a8d2e2a7465f586
di3go-sona <diego.sonaglia@gmail.com>
2021-09-22 14:45:55 +0200

    changed app structure, moved timeline to new index

--- app.py
+++ app.py
@@ -1,37 +1,38 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import json 
 import loader
-
-
+from context_loader import ctx
+from wallet_clustering import * 
 app = Flask(__name__, static_folder='static/', template_folder='templates/')
 
-# @app.route("/")
-# def index():
-#     return render_template('index.html.j2')
-
-min_pca_1, max_pca_1, min_pca_2, max_pca_2 = loader.get_wallets_domain()
-
-ctx={
-    'min_pca_1': min_pca_1,
-    'max_pca_1': max_pca_1,
-    'min_pca_2': min_pca_2,
-    'max_pca_2': max_pca_2
-}
 
 @app.route("/")
-@app.route("/timeline")
-def timeline():
-    return render_template('timeline.html.j2',ctx=ctx)
+def index():
+    return render_template('index.html.j2',ctx=ctx)
 
 @app.route("/timeline/csv")
 def timeline_csv_base():
     param = request.args.get("param")
     return loader.get_blocks(param)
 
-@app.route("/clusters/csv")
-def clusters_csv_base():
-    param = request.args.get("param")
-    return loader.get_wallets(param)
+@app.route("/wallets/csv")
+def wallets_csv_base():
+    blocks_list = request.args.get("blocks_list")
+    return loader.get_wallets(blocks_list)
+
+
+
+@app.route("/wallets/clusters/start")
+def wallets_clusters_new():
+    blocks_list = request.args.get("blocks_list")
+    start_clustering()
+    return redirect('/wallets/clusters/csv')
+
+@app.route("/wallets/clusters/csv")
+def wallets_clusters_csv_base():
+    blocks_list = request.args.get("blocks_list")
+    return get_clustering()
+
 
 # @app.route("/blocks_info")
 # def get_blocks_info(): 
